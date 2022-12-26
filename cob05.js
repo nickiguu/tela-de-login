d98d6eca47186738c7362ecb9eb01afd5e31cda2
igor <113356517+nickiguu@users.noreply.github.com>
2022-12-26 14:02:26 -0300

    Add files via upload

---
+++ cob05.js
@@ -0,0 +1,22 @@
+
+
+ function validacao (){
+   if (lugarnome.value === "teste") {
+     console.log("correto")
+   } else {
+    console.log("usuario incorreto");
+   }
+ } 
+ 
+ 
+ 
+ 
+ 
+/* document.getElementById("button01").onclick = function() {
+
+ if(lugarnome == "joao") {
+   console.log("correto");
+ } else {
+   console.log("errado");
+ }
+} */
